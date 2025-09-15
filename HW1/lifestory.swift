b2aeb0e3588919a3a7c0151873f1496184bbfad8
ushyndali <uldana.shyndali@prowheelxr.com>
2025-09-15 22:50:39 +0500

    HW 1 edited

--- HW1/lifestory.swift
+++ HW1/lifestory.swift
@@ -12,9 +12,11 @@ let numberOfHobbies = 3
 let favoriteNumber = 5
 let isHobbyCreative = true
 
+let interestingFact = "I have visited the northerst McDonalds in the world."
+
 let lifeStory = "My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). " 
 + (isStudent ? "I am currently a student. " : "") 
 + "I enjoy \(hobby), which is " 
 + (isHobbyCreative ? "a creative" : "not a creative")
-+ " hobby. I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber)."
++ " hobby. I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber). \(interestingFact)"
 print(lifeStory)
