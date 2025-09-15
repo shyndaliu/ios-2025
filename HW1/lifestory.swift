49bd2dc5df1a77609836dbd7bea23c31e9e81a96
ushyndali <uldana.shyndali@prowheelxr.com>
2025-09-15 22:48:20 +0500

    HW 1

---
+++ HW1/lifestory.swift
@@ -0,0 +1,20 @@
+let firstName = "Uldana"
+let lastName = "Shyndali"
+let isStudent = true
+let height = 168
+let birthYear = 2005
+let currentYear = 2025
+let age = currentYear - birthYear
+
+
+let hobby = "video editing"
+let numberOfHobbies = 3
+let favoriteNumber = 5
+let isHobbyCreative = true
+
+let lifeStory = "My name is \(firstName) \(lastName). I am \(age) years old, born in \(birthYear). " 
++ (isStudent ? "I am currently a student. " : "") 
++ "I enjoy \(hobby), which is " 
++ (isHobbyCreative ? "a creative" : "not a creative")
++ " hobby. I have \(numberOfHobbies) hobbies in total, and my favorite number is \(favoriteNumber)."
+print(lifeStory)
